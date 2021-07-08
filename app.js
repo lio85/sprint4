b9430f0927af7cb1fff6128eac75aa96e6477363
lio85 <lionelprats2021@gmail.com>
2021-07-08 01:46:05 -0300

    procfile con P mayúscula y agregue la 3era. linea para tratar de que funcione heroku

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const userRoutes = require('./src/routes/userRoutes');
     console.log('Servidor 3000 corriendo');
 })*/
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo en el puerto 3000');
-});
+})
 
 
 app.use(express.static(path.resolve(__dirname , './public')));
